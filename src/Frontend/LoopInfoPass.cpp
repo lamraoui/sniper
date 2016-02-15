9517f205218af73f25746464799ab517e48197ff
Si-Mohamed Lamraoui <simo.nii.public@gmail.com>
2016-02-15 15:19:29 +0900

    Adding documentation for LoopInfoPass class.

--- src/Frontend/LoopInfoPass.cpp
+++ src/Frontend/LoopInfoPass.cpp
@@ -1,12 +1,8 @@
 /**
- * LoopInfoPass.cpp
+ * \file LoopInfoPass.cpp
  *
- * 
- *
- * @author : Si-Mohamed Lamraoui
- * @contact : simo@nii.ac.jp
- * @date : 2011/05/31
- * @copyright : NII 2012
+ * \author Si-Mohamed Lamraoui
+ * \date   15 February 2015
  */
 
 #include "LoopInfoPass.h"
@@ -14,17 +10,9 @@
 
 char LoopInfoPass::ID = 0;
 
-
-LoopInfoPass::LoopInfoPass(Module *_llvmMod, bool _dbgMsg) : FunctionPass(ID) {
-    this->llvmMod = _llvmMod;
-    this->dbgMsg = _dbgMsg;
-}
-
-
 Pass *LoopInfoPass::createLoopInfoPass(Module *llvmMod, bool dbgMsg) { 
     return new LoopInfoPass(llvmMod, dbgMsg); 
 }
-
 
 void LoopInfoPass::assertLoop(Loop *l) {
     
@@ -164,69 +152,3 @@ bool LoopInfoPass::runOnFunction(Function &F) {
     MSG("=========================================\n");
     return false;
 }
-
-
-/*bool LoopInfoPass::runOnFunction(Function &F) {
-    
-    bool changed = false;
-    LoopInfo *li = &getAnalysis<LoopInfo>();
-    
-    if(!this->dbgMsg)
-        return false;
-    
-    for (LoopInfo::iterator I = li->begin(), E = li->end(); I != E; ++I) {
-        Loop *l = *I; 
-        std::cout << "- Loop:\n";
-        // Tell if there are loops contained entirely within this loop
-        if(l->getSubLoops().empty()) {
-            std::cout << "  no subloops\n";
-        } else {
-            std::cout << "  has " << l->getSubLoops().size() 
-            << " subloops\n";
-        }
-        // Number of basic blocks which make up this loop
-        if(!l->getBlocks().empty()) {
-            std::cout << "  " << l->getBlocks().size() << " basic blocks\n";
-        }
-        // Number of back edges to the loop header
-        std::cout << "  " << l->getNumBackEdges() << " back edges\n";
-        // If getExitingBlocks would return exactly one block,
-        // return that block. Otherwise return null
-        if(l->getExitingBlock()) {
-            std::string s = l->getExitingBlock()->getName();
-            std::cout << "  " << s << " is the ExitingBlock\n";
-        } else
-            std::cout << "  there are more than one ExitingBlock\n";
-    
-        // Aloop has a preheader if there is only one edge to the header of 
-        // the loop from outside of the loop.  If this is the case, 
-        // the block branching to the header of the loop is the preheader node.
-        if(!l->getLoopPreheader())
-            std::cout << "  no preheader\n";
-        else {
-            std::string s = l->getLoopPreheader()->getName();
-            std::cout << "  " << s << " is the preheader\n";
-        }
-        // If the given loop's header has exactly one unique
-        // predecessor outside the loop, return it. Otherwise return null.
-        // This is less strict that the loop "preheader" concept, which requires
-        // the predecessor to have exactly one successor.
-        if(!l->getLoopPredecessor())
-            std::cout << "  no predecessor\n";
-        else {
-            std::string s = l->getLoopPredecessor()->getName();
-            std::cout << "  " << s << " is the predecessor\n";
-        }
-        // If there is a single latch block for this loop, return it.
-        // A latch block is a block that contains a branch back to the header.   
-        if(!l->getLoopLatch())
-            std::cout << "  no latch block\n";
-        else {
-            std::string s = l->getLoopLatch()->getName();
-            std::cout << "  " << s << " is a latch block\n";
-        }
-        
-        
-    }
-    return changed;
-}*/
--- src/Frontend/LoopInfoPass.h
+++ src/Frontend/LoopInfoPass.h
@@ -1,12 +1,8 @@
 /**
- * LoopInfoPass.h
+ * \file LoopInfoPass.h
  *
- * 
- *
- * @author : Si-Mohamed Lamraoui
- * @contact : simo@nii.ac.jp
- * @date : 2013/07/31
- * @copyright : NII 2013
+ * \author Si-Mohamed Lamraoui
+ * \date   15 February 2015
  */
 
 #ifndef LOOPINFOPASS_H
@@ -47,36 +43,105 @@
 
 using namespace llvm;
 
-
-//============================================================================
+/**
+ * \class LoopInfoPass
+ *
+ * This class is an LLVM pass that processes loops in the target function.
+ */
 class LoopInfoPass : public FunctionPass {
     
+    /**
+     * LLVM pass ID.
+     */
     static char ID;
+
+    /**
+     * Set required information for the pass
+     * (see LLVM Pass documentation for details).
+     */
     virtual void getAnalysisUsage(AnalysisUsage &Info) const {
         Info.addRequired<LoopInfo>();
         Info.addPreserved<LoopInfo>();
     }
     
 private:
+    /**
+     * Target LLVM module.
+     */
     Module *llvmMod;
+    /**
+     * True if debug message are shown, false otherwise.
+     */
     bool dbgMsg;
-    Function *assertFun;
+    /**
+     * Map LLVM branch instructions to loop header basic block.
+     * These branch instructions jump to the loop headers. 
+     */
     std::map<BranchInst*, BasicBlock*> br2header;
     
 public:
-    LoopInfoPass(Module *_llvmMod, bool _dbgMsg);
+    LoopInfoPass(Module *_llvmMod, bool _dbgMsg) 
+    : llvmMod(_llvmMod), dbgMsg(_dbgMsg), FunctionPass(ID) { }
     ~LoopInfoPass() { }
+
+    /**
+     * Return an instance of this class.
+     * 
+     * \param llvmMod The target LLVM module.
+     * \param dbgMsg True if debug message are shown, false otherwise.
+     * \return An instance of this LoopInfoPass. 
+     */
     Pass *createLoopInfoPass(Module *llvmMod, bool dbgMsg);
+    /**
+     * LLVM executes this function on each function 
+     * of the target LLVM module.
+     *
+     * \param F A LLVM function to be processed by the pass. 
+     * \return true if the pass modified the the function \p F.
+     */
     virtual bool runOnFunction(Function &F);
+    /**
+     * Return the latch basic block associated to the given branch instruction.
+     * The branch instruction \p br jump to either a basic block B1 or B2. 
+     * This function returns either B1 or B2.
+     * For recall, a latch basic block is a basic block that unconditionally jumps 
+     * to a the loop header basic block. 
+     *
+     * \param br An LLVM branch instruction.
+     * \return A latch basic block.
+     */
     BasicBlock* getLoopLatch(BranchInst *br);
+    /**
+     * Returns the basic block's name of the latch basic block 
+     * associated with the given branch instruction 
+     * (see getLoopLatch function for details).
+     * 
+     * \param br An LLVM branch instruction.
+     * \return  A latch basic block's name.
+     */
     std::string getLoopLatchName(BranchInst *br);
+    /**
+     * Return true if \p bb is a latch basic block.
+     */
     bool isLoopLatch(BasicBlock *bb);
+    /**
+     * Return all LLVM branch instruction associated 
+     * with latch basic blocks.
+     */
     std::vector<BranchInst*> getLoopLatchBrs();
+    /**
+     * Return true if the target LLVM module contains loops. 
+     */
     bool hasLoops();
     
 private:
+    /**
+     * Collect and save latch basic blocks in the loop \p l.
+     * TODO: remove back edge and add sniper_assert(false) function 
+     * to show that the loop \p l was not unroll enough in case the 
+     * assert is reached.
+     */
     void assertLoop(Loop *l);
 };
-//============================================================================
 
-#endif
+#endif // LOOPINFOPASS_H
