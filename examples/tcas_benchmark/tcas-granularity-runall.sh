e02facdf06937906589ab599cbe7955dfa6e7e8f
Si-Mohamed Lamraoui <simo.nii.public@gmail.com>
2016-03-18 12:37:11 +0900

    Cleaning and adding comments.

--- examples/tcas_benchmark/tcas-granularity-runall.sh
+++ examples/tcas_benchmark/tcas-granularity-runall.sh
@@ -1,48 +1,65 @@
 #!/bin/bash
 
+#====================================================
+# Experiment for SNIPER on the TCAS Benchmark
+# 
+# This experiment aims at comparing the results of 
+# SNIPER with FFTF with different granularity levels.
+#
+# @author  Si-Mohamed Lamraoui
+# @date    18 March 2016
+#====================================================
+
 NB_RUN=1
 TIMEFORMAT='Time: %3U + %3S';
+
+function run {
+    if [ "$2" == "inst" ]; then
+        GRANU_LEVEL=-inst
+    elif [ "$2" == "line" ]; then
+        GRANU_LEVEL=-line
+    else
+        GRANU_LEVEL=-block
+    fi
+    clang-mp-3.3 $1 -S -emit-llvm -O0 -gline-tables-only -o $1.bc >error 2>&1
+    time ./../../src/sniper -ce $GRANU_LEVEL -function foo -cfile $1 -loc 173 -ts testcases-argok.txt -go golden-outputs.txt $1.bc
+    rm $1.bc
+}
+
+# Inst-level
+function runFFTFi {
+    echo ""
+    echo "FFTF (inst-lvl) < $1 >"
+     run $1 "inst"
+}
 
 # Line-level
 function runFFTFl {
     echo ""
     echo "FFTF (line-lvl) < $1 >"
-    clang-mp-3.3 $1 -S -emit-llvm -O0 -gline-tables-only -o $1.bc >error 2>&1
-    time ./../../src/sniper -ce -line -function foo -cfile $1 -loc 173 -ts testcases-argok.txt -go golden-outputs.txt $1.bc
-    rm $1.bc
+    run $1 "line"
 }
+
 # Block-level
 function runFFTFb {
     echo ""
     echo "FFTF (block-lvl) < $1 >"
-    clang-mp-3.3 $1 -S -emit-llvm -O0 -gline-tables-only -o $1.bc >error 2>&1
-    time ./../../src/sniper -ce -block -function foo -cfile $1 -loc 173 -ts testcases-argok.txt -go golden-outputs.txt $1.bc
-    rm $1.bc
+     run $1 "block"
 }
-# Block-level
-function runFFTFi {
-    echo ""
-    echo "FFTF (inst-lvl) < $1 >"
-    clang-mp-3.3 $1 -S -emit-llvm -O0 -gline-tables-only -o $1.bc >error 2>&1
-    time ./../../src/sniper -ce -inst -function foo -cfile $1 -loc 173 -ts testcases-argok.txt -go golden-outputs.txt $1.bc
-    rm $1.bc
-}
-
-
 
 # Run all TCAS programs for both type of formula
 echo "Running all TCAS versions with time()..."
+echo ""
 
-
-echo "== FFTF (line, block, inst) ===================================="
+echo "== FFTF (inst, line, block) ===================================="
 for i in $(eval echo {1..41})
 do
-    echo "======================================"
-    runFFTFb v$i/tcas.c
+    runFFTFi v$i/tcas.c
     echo ' '
     runFFTFl v$i/tcas.c
     echo ' '
-    runFFTFi v$i/tcas.c
+    runFFTFb v$i/tcas.c
+    echo "======================================"
 done
 echo ' '
 
