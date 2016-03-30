85e911c387510872d90dd853f7c20e890212bcf6
Si-Mohamed Lamraoui <simo.nii.public@gmail.com>
2016-03-30 11:26:47 +0900

    Adding GNU GPL v3 header to all source files.

--- examples/bekkouche_benchmark/runall-time.sh
+++ examples/bekkouche_benchmark/runall-time.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-#====================================================
+# runall-time.sh
+#
+# ----------------------------------------------------------------------
+#
 # Experiment for SNIPER on the Bekkouche Benchmark
 #
 # This experiment aims at comparing the results of
 # SNIPER with different trace formula encodings.
 #
-# @author  Si-Mohamed Lamraoui
-# @date    18 March 2016
-#====================================================
+# ----------------------------------------------------------------------
+#                SNIPER : Automatic Fault Localization 
+#
+# Copyright (C) 2016 Si-Mohamed LAMRAOUI
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program (see LICENSE.TXT).  
+# If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+#
+# \author Si-Mohamed LAMRAOUI
+# \date   18 March 2016
+
 
 # Number of time each program is analysed to
 # have an average computing time.
--- src/ExecutionEngine/ConcolicProfiler.cpp
+++ src/ExecutionEngine/ConcolicProfiler.cpp
@@ -1,6 +1,26 @@
 /**
  * \file ConcolicProfiler.cpp
  *
+ * ----------------------------------------------------------------------
+ *                SNIPER : Automatic Fault Localization 
+ *
+ * Copyright (C) 2016 Si-Mohamed LAMRAOUI
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see LICENSE.TXT).  
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------
+ *
  * \author Si-Mohamed Lamraoui
  * \date   24 February 2016
  */
